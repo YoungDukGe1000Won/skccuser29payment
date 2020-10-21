666d89258588e0d0b18a603d2b8aca4214e374ed
YoungDukGe1000Won <yoduli123@gmail.com>
2020-10-21 18:38:43 +0900

    1st commit..

---
+++ src/main/java/skccuser/PayCancelled.java
@@ -0,0 +1,42 @@
+package skccuser;
+
+public class PayCancelled extends AbstractEvent {
+
+    private Long id;
+    private Long orderId;
+    private String productId;
+    private Integer qty;
+
+    public PayCancelled(){
+        super();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+    public Integer getQty() {
+        return qty;
+    }
+
+    public void setQty(Integer qty) {
+        this.qty = qty;
+    }
+}
---
+++ src/main/java/skccuser/PaymentCancellationRepository.java
@@ -0,0 +1,8 @@
+package skccuser;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface PaymentCancellationRepository extends PagingAndSortingRepository<PaymentCancellation, Long>{
+
+
+}
